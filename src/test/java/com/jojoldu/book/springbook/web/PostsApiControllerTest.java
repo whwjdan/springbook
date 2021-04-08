b9550be351f4885ed222eadc0c5175ba060745f4
whwjdan <whwjdan@naver.com>
2021-04-08 04:21:22 +0000

    service, dto, apicontroller 추가 및 테스트

---
+++ src/test/java/com/jojoldu/book/springbook/web/PostsApiControllerTest.java
@@ -0,0 +1,68 @@
+package com.jojoldu.book.springbook.web;
+
+import com.jojoldu.book.springbook.domain.posts.Posts;
+import com.jojoldu.book.springbook.domain.posts.PostsRepository;
+import com.jojoldu.book.springbook.web.dto.PostsSaveRequestDto;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.
+        WebEnvironment.RANDOM_PORT)
+public class PostsApiControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @AfterEach
+    public void tearDown() throws Exception{
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void Posts_등록된다() throws Exception{
+        //given
+        String title = "title";
+        String content = "content";
+        PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
+                .title(title)
+                .content(content)
+                .author("author")
+                .build();
+
+        String url = "http://localhost:"+port+"/api/v1/posts";
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate
+                .postForEntity(url, requestDto, Long.class);
+        //then
+        assertThat(responseEntity.getStatusCode()).
+                isEqualTo(HttpStatus.OK);
+
+        assertThat(responseEntity.getBody()).
+                isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
+        assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+}
