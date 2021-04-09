d0c791189378302a4ec5c1cebee459657ff73ff9
whwjdan <whwjdan@naver.com>
2021-04-09 20:39:20 +0900

    update, select 추가 및 테스트

--- src/main/java/com/jojoldu/book/springbook/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/springbook/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.jojoldu.book.springbook.service.posts;
 
+import com.jojoldu.book.springbook.domain.posts.Posts;
 import com.jojoldu.book.springbook.domain.posts.PostsRepository;
+import com.jojoldu.book.springbook.web.dto.PostsResponseDto;
 import com.jojoldu.book.springbook.web.dto.PostsSaveRequestDto;
+import com.jojoldu.book.springbook.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,5 +19,23 @@ public class PostsService {
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity())
                                     .getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new
+            IllegalArgumentException("해당 게시글이 없습니다. id ="+id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new
+                        IllegalArgumentException("해당 게시글이 없습니다. id ="+id));
+
+        return new PostsResponseDto(entity);
     }
 }
